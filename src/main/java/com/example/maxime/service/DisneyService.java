f4a03e15db1de30a89a2e9b75c1945e4885d81d2
Maxime <marchal.maxime@live.fr>
2021-01-18 22:24:04 +0100

    disney

--- src/main/java/com/example/maxime/service/DisneyService.java
+++ src/main/java/com/example/maxime/service/DisneyService.java
@@ -5,9 +5,11 @@ import com.example.maxime.dto.DisneyDto;
 import com.example.maxime.entities.Disney;
 import com.example.maxime.repository.DisneyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
+@Service
 public class DisneyService {
 
     @Autowired
